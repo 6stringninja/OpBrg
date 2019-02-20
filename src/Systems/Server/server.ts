33ec07bf0e5a5f0c6586ebafecaea5d815b18c98
doug2691 <dougreuter@gmail.com>
2019-02-20 17:50:34 -0600

    server.ts cleanup

--- src/Systems/Server/server.ts
+++ src/Systems/Server/server.ts
@@ -5,48 +5,45 @@ import { IServerConfig } from './ServerConfig';
 import { container } from 'tsyringe';
 import { ServerDi } from './ServerDi';
 import { ServerState } from './ServerState';
-import {
-  CreateClientMessageInput,
-  CreateClientMessageResult,
-  CreateClientMessageWrapper
-} from './Messages/CreateClientMessage';
-import { ApplicationClientCreateResult } from '../Application/ApplicationClientCreateResult';
 import { IMessageWrapper } from './Messages/Base/MessageWrapperBase';
 import { Messages } from './Messages/index';
+
 export class Server {
   app: express.Application = express();
   serverState: ServerState | undefined;
   serverMessages: IMessageWrapper[] = [];
   serverRoutePrefix = 'api';
   listener: import('http').Server | undefined;
+
   constructor(public config: IServerConfig = ServerConfig as IServerConfig) {
     new ServerDi().load(container);
-
     this.serverState = ServerState.create(config.serverPassword);
     if (!this.serverState.loadAll())
       throw new Error('failed to load json files');
   }
   private getApiUrl = (name: string) => `/${this.serverRoutePrefix}/${name}`;
- async initMessages() {
+
+  async initMessages() {
     this.serverMessages = Messages(this.serverState);
     this.serverMessages.forEach(m => {
-
-    this.app.post(this.getApiUrl(m.name), (req: express.Request, res: express.Response) =>
-      m.processExpress(req, res));
+      this.app.post(
+        this.getApiUrl(m.name),
+        (req: express.Request, res: express.Response) =>
+          m.processExpress(req, res)
+      );
     });
-
   }
- async start() {
+
+  async start() {
     this.app.use(express.json());
-  await this.initMessages();
-
-
-  this.listener =  this.app.listen(this.config.port, () => {
+    await this.initMessages();
+    this.listener = this.app.listen(this.config.port, () => {
       console.log(
         `OpBorg listening on port http://localhost:${this.config.port}/ !`
       );
     });
   }
+
   async stop() {
     if (this.listener) {
       this.listener.close();
